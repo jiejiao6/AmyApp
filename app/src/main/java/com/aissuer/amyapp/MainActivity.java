2f26b5b1d8c564f53a49cafb07935badae2ba74c
amyMeng <393952590@qq.com>
2018-07-31 19:50:21 +0800

    first commit

---
+++ app/src/main/java/com/aissuer/amyapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.aissuer.amyapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false));
+    }
+}
